9e61b4b153768551003326a6a5f6194c84273add
lucnguyen0611 <ntluc.work@gmail.com>
2025-06-13 15:18:23 +0700

    Bstar 4

--- Bkstarstudy/web/src/pages/ClassOverView/index.tsx
+++ Bkstarstudy/web/src/pages/ClassOverView/index.tsx
@@ -2,12 +2,13 @@ import {MainLayout} from '../../components'
 import {Members, Overview, Tests} from "../../components/ClassroomItem";
 import type {Classroom} from '../../utils'
 import {useLocation} from "react-router-dom";
-import {useState} from "react";
+// import {useState} from "react";
 
 
 export default function ClassOverview() {
     const location = useLocation();
-    const [classroom, setClassroom] = useState<Classroom>(location.state);
+    // const [classroom, setClassroom] = useState<Classroom>(location.state);
+    const classroom: Classroom = location.state;
 
     return (
         <>
